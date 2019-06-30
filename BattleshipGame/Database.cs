5bcc8a751f4f8ef4102eedd878c3a2425e93d658
SketheR <harangi.lajos.1996@gmail.com>
2019-06-30 18:38:48 +0200

    Added PlayerStatistics and calculations

--- BattleshipGame/Database.cs
+++ BattleshipGame/Database.cs
@@ -24,6 +24,20 @@ namespace BattleshipGame
         }
     }
 
+    public class PlayerStatistics
+    {
+        public string Name { get; private set; }
+        public int Won { get; set; }
+        public int Lost { get; set; }
+        public int Played { get { return Won + Lost; } }
+        public double WinRate { get { return Won / (double)Played; } }
+
+        public PlayerStatistics(string name)
+        {
+            Name = name;
+        }
+    }
+
     public static class Database
     {
         private const string dbFile = "history.db";
@@ -44,6 +58,20 @@ namespace BattleshipGame
                 LiteCollection<MatchResult> collection = db.GetCollection<MatchResult>("History");
                 return collection.FindAll().OrderByDescending(x => x.PlayedAt);
             }
+        }
+
+        public static IEnumerable<PlayerStatistics> GetLeaderBoard()
+        {
+            Dictionary<string, PlayerStatistics> stats = new Dictionary<string, PlayerStatistics>();
+            foreach (MatchResult item in GetHistory())
+            {
+                if (stats.ContainsKey(item.Winner)) stats[item.Winner].Won++;
+                else stats.Add(item.Winner, new PlayerStatistics(item.Winner) { Won = 1});
+
+                if (stats.ContainsKey(item.Looser)) stats[item.Looser].Lost++;
+                else stats.Add(item.Looser, new PlayerStatistics(item.Looser) { Lost = 1 });
+            }
+            return stats.Values.ToList<PlayerStatistics>().OrderByDescending(x=> x.WinRate).ThenBy(y=> y.Name);
         }
     }
 }
